1ae709ee185854219eef7459907a9750a90b9fd4
leo <leomb_punk@hotmail.com>
2017-06-24 18:05:24 -0300

    asd

--- matrices en c/matrices en c/matrices en c.cpp
+++ matrices en c/matrices en c/matrices en c.cpp
@@ -7,10 +7,10 @@
 
 using namespace std;
 
-#define N 4 //dimension para una matriz cuadrada NxN
-#define M 5 //dimension para una matriz no cuadrada NxM
+#define N 3 //dimension para una matriz cuadrada NxN
+#define M 2 //dimension para una matriz no cuadrada NxM
 
-int matriz[N][N];
+int matriz[N][N], matriz1[M][M], matriz2[M][N], matriz3[M][N];
 
 void main(void)
 {
@@ -66,5 +66,98 @@ void main(void)
 			cout << matriz[f][c] << ", ";
 		}
 	}
+	//transpuesta de una matriz
+	cout << "transpuesta:" << endl;
+	for (int x = 0; x < N; x++) {
+		for (int y = 0; y < N; y++) {
+			cout << matriz[y][x] << ", ";
+		}
+		cout << "\n";
+	}
+	//producto de matrices
+	//producto=fila(toda) * columna(toda)
+	//la cantidad de elementos de la fila de la matriz1 debe ser igual a los elementos de la 
+	//columna de la matriz2 para poder obtener el producto matriz3
+	//multiplicacion miembro por miembro
+	//carga
+	for (int x = 0; x < M; x++)
+	{
+		for (int y = 0; y < M; y++)
+		{
+			matriz1[x][y] = rand() % 10;
+		}
+	}
+
+	for (int x = 0; x < M; x++)
+	{
+		for (int y = 0; y < N; y++)
+		{
+			matriz2[x][y] = rand() % 10;
+		}
+	}
+	//cargo la 3 con ceros porque es una hija de.... y me tira cualquiera
+	for (int x = 0; x < M; x++)
+	{
+		for (int y = 0; y < N; y++)
+		{
+			matriz3[x][y] = 0;
+		}
+	}
+	//muestro
+	cout << "matriz1" << endl;
+	for (int x = 0; x < M; x++)
+	{
+		for (int y = 0; y < M; y++)
+		{
+			cout << matriz1[x][y] << ", ";
+			//system("pause");
+		}
+		cout << "\n";
+	}
+	cout << "matriz2" << endl;
+	for (int x = 0; x < M; x++)
+	{
+		for (int y = 0; y < N; y++)
+		{
+			cout << matriz2[x][y] << ", ";
+		}
+		cout << "\n";
+	}
+	//producto de matrices ---> con 3 for?
+	for (int f = 0; f < M; f++)
+	{
+		for (int c = 0; c < M; c++)
+		{
+			for (int z = 0; z < N; z++)
+			{
+				matriz3[f][z] += (matriz1[f][c] * matriz2[c][z]);
+				//cout << "indices " << f << "," << c << "    " << c << "," << z << endl;
+				//cout << matriz1[f][c] << "*" << matriz2[c][z] << endl;
+			}
+		}
+	}
+	//muestro
+	cout << "matriz3" << endl;
+	for (int x = 0; x < M; x++)
+	{
+		for (int y = 0; y < N; y++)
+		{
+			cout << matriz3[x][y] << ", ";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+	//-----------------------------------------------------------------------------------------------
+	//matriz tipo char
+	char alfabeto[27]; // 26 letras más NULL
+	char letra;
+	int i;
+
+	for (letra = 'A', i = 0; letra <= 'Z'; letra++, i++)//interesante for
+		alfabeto[i] = letra;
+
+	alfabeto[i] = NULL;
+	cout << "Las letras son: " << alfabeto << endl;
+
 	system("pause");
 }
